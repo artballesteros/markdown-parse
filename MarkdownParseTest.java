ac2067fc3187d69b2b4c9e43eabb59e4851ff9ff
artballesteros <58927442+artballesteros@users.noreply.github.com>
2022-01-27 16:03:36 -0800

    first getLinks test passing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void readFiles() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        List links = List.of("https://something.com","some-page.html");
+        assertEquals("check results", links, MarkdownParse.getLinks(contents));
+    }
+
 }
